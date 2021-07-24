235ad6679d0192a7dd3fa934e359b91dc02bb478
Krankenkopf <petricowork@gmail.com>
2021-07-24 23:18:32 +0300

    api > api for local server created (not include it to master branch)

---
+++ src/Api/local-api.ts
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+export const instance = axios.create(
+    {
+        baseURL: 'http://localhost:7542/2.0/',
+        withCredentials: true,
+        /*  headers: {
+              'API-KEY': ''
+          }*/
+
+    }
+)
