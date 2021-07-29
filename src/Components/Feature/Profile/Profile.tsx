509cf4c813402d515c4857a0005a98f70fd267fe
Dmitry Kelek <kelekofficial@gmail.com>
2021-07-29 12:25:23 +0300

    Merge Vadim branch

--- src/Components/Feature/Profile/Profile.tsx
+++ src/Components/Feature/Profile/Profile.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import S from './Profile.module.css'
 import {useDispatch, useSelector} from 'react-redux';
 import {AppStoreType} from '../../../Store/store';
-import {UserDataType} from '../../../API/authAPI';
+import {UserDataType} from '../../../Api/authAPI';
 import {Redirect} from 'react-router-dom';
 import {logout} from '../../../Store/auth-reducer';
 import {Button} from '@material-ui/core';
@@ -11,36 +11,36 @@ type ProfilePropsType = {}
 
 export const Profile: React.FC<ProfilePropsType> = props => {
 
-  const userData = useSelector<AppStoreType, UserDataType | null>(state => state.auth.userData)
-  const dispatch = useDispatch()
+    const userData = useSelector<AppStoreType, UserDataType | null>(state => state.auth.userData)
+    const dispatch = useDispatch()
 
-  const logoutHandler = () => {
-    dispatch(logout())
-  }
+    const logoutHandler = () => {
+        dispatch(logout())
+    }
 
 
-  if(userData === null) {
-    return <Redirect to='/login'/>
-  }
+    if (userData === null) {
+        return <Redirect to='/login'/>
+    }
 
-  return (
-    <div className={S.profile}>
-      <div className={S.profileWrap}>
-        <h4>Profile page!</h4>
-        <div className={S.avatar}>
-          <img src={userData.avatar} alt='avatar'/>
+    return (
+        <div className={S.profile}>
+            <div className={S.profileWrap}>
+                <h4>Profile page!</h4>
+                <div className={S.avatar}>
+                    <img src={userData.avatar} alt='avatar'/>
+                </div>
+                <div className={S.name}>
+                    <p>
+                        {userData.name}
+                    </p>
+                </div>
+                <div>
+                    <Button className={S.button} color={'primary'}
+                            variant={'contained'}
+                            onClick={logoutHandler}>Logout</Button>
+                </div>
+            </div>
         </div>
-        <div className={S.name}>
-          <p>
-            {userData.name}
-          </p>
-        </div>
-        <div>
-          <Button className={S.button} color={'primary'}
-                  variant={'contained'}
-                  onClick={logoutHandler}>Logout</Button>
-        </div>
-      </div>
-    </div>
-  )
+    )
 }
