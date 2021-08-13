aca37a80b87c6b8a5723cd8c837360fb0aa4d24c
Syava7 <syava77787@gmail.com>
2021-08-13 17:51:35 +0300

    update

--- src/Store/store.ts
+++ src/Store/store.ts
@@ -5,13 +5,17 @@ import {AuthActionsType, authReducer} from "./auth-reducer";
 import {RegistrationActionsType, registrationReducer} from "./registration-reducer";
 import {RecoveryPassActionsType, recoveryPassReducer} from "./recovery-pass-reducer";
 import {AppActionsType, appReducer} from "./app-reducer";
+import {decksSlice} from "./decks-reducer";
+import {cardsSlice} from "./cards-reducer";
 
 const rootReducer = combineReducers({
-    profile: profileReducer,
-    auth: authReducer,
-    registration: registrationReducer,
-    recovery: recoveryPassReducer,
-    app: appReducer
+  profile: profileReducer,
+  auth: authReducer,
+  registration: registrationReducer,
+  recovery: recoveryPassReducer,
+  app: appReducer,
+  decks: decksSlice.reducer,
+  cards: cardsSlice.reducer
 })
 
 export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware))
@@ -20,11 +24,16 @@ export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware))
 export type AppStoreType = ReturnType<typeof rootReducer>
 export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, AppStoreType, unknown, AllAppActionsType>
 export type AllAppActionsType =
-    ProfileActionsType
-    | AuthActionsType
-    | RegistrationActionsType
-    | RecoveryPassActionsType
-    | AppActionsType
-
+  ProfileActionsType
+  | AuthActionsType
+  | RegistrationActionsType
+  | RecoveryPassActionsType
+  | AppActionsType
+export type AppDispatchType = typeof store.dispatch
+export type ThunkApiType = {
+  dispatch: AppDispatchType,
+  state: AppStoreType,
+  rejectValue: string
+}
 //@ts-ignore
 window.store = store
